6087a18cd1a3f52799b12c9c2e32b35cb9f73656
Daniel Singer <zigius@gmail.com>
2023-06-18 15:52:28 +0300

    WIP

--- lua/telescope/_extensions/links_picker.lua
+++ lua/telescope/_extensions/links_picker.lua
@@ -4,23 +4,28 @@ local finders = require("telescope.finders")
 local pickers = require("telescope.pickers")
 local conf = require("telescope.config").values
 
+
 return require("telescope").register_extension {
   exports = {
     insert_link = function(opts)
       opts = opts or {}
 
       pickers.new(opts, {
-          prompt_title = "links",
-          finder = finders.find_files,
-          sorter = conf.generic_sorter(opts),
-          attach_mappings = function(prompt_bufnr, map)
-            actions.select_default:replace(function()
-              actions.close(prompt_bufnr)
-              local selection = action_state.get_selected_entry()
-              vim.api.nvim_put({ "[[" .. "test text" .. "]]" }, "", false, true)
-            end)
-            return true
-          end,
+        prompt_title = "links",
+        finder = finders.new_oneshot_job({ "rg", "--files" }, opts),
+        sorter = conf.file_sorter(opts),
+        previewer = conf.file_previewer(opts),
+        attach_mappings = function(prompt_bufnr, map)
+          actions.select_default:replace(function()
+            actions.close(prompt_bufnr)
+            local selection = action_state.get_selected_entry()
+            P(selection[1])
+            local cwd = vim.fn.getcwd()
+            P(cwd)
+            -- vim.api.nvim_put({ "'" .. selection[1] .. "'," }, "", false, true)
+          end)
+          return true
+        end,
       }):find()
     end
   }
